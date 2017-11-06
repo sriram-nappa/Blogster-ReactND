2e3fc297ff6cb1cd729526642b8142086f2ad5cb
Sriram Poondi Chinappa <2k6sriram@gmail.com>
2017-11-05 18:47:28 -0700

    Initial commit with folder structure and reducers

---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+import { Provider } from 'react-redux;'
+
+ReactDOM.render(
+    <Provider>
+        <BrowserRouter><App /></BrowserRouter>
+    </Provider>, document.getElementById('root'));
+registerServiceWorker();
---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,11 @@
+import {combineReducers} from 'redux';
+
+import categoryReducer from './categoryReducer';
+import postReducer from './postReducer';
+import commentReducer from './commentReducer';
+
+export default combineReducers({
+    categories: categoryReducer,
+    posts: postReducer,
+    comments: commentReducer
+})
