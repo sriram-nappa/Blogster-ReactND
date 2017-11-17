67427961465ed170dbb7f0bd12866f9a1afdb9e5
Sriram Poondi Chinappa <2k6sriram@gmail.com>
2017-11-16 23:59:41 -0700

    Adds css and category List

--- frontend/src/components/categoryList.js
+++ frontend/src/components/categoryList.js
@@ -3,12 +3,25 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Modal from 'react-modal';
+import {List, ListItem} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import './categoryList.css'
 
 class CategoryList extends Component {
     render() {
+        const {categories} = this.props
+        const categoriesWrapper = categories.map((category, i) => {
+            <div key={i}>
+                {category.name}
+            </div>
+        });
         return(
             <div className="categoryList">
                 Category List
+                <Subheader inset={true}>Folders</Subheader>
+                <List>
+                    {categoriesWrapper}
+                </List>
             </div>
         );    
     }
