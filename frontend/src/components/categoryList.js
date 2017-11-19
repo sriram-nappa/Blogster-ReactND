94c0e522e3f0cfa964f6b8340e1410219e254bfb
Sriram Poondi Chinappa <2k6sriram@gmail.com>
2017-11-18 23:19:46 -0700

    Adds Grid style for categories

--- frontend/src/components/categoryList.js
+++ frontend/src/components/categoryList.js
@@ -4,22 +4,45 @@ import { withRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Modal from 'react-modal';
 import {List, ListItem} from 'material-ui/List';
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
 import Subheader from 'material-ui/Subheader';
 import './categoryList.css'
 
+const styles = {
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+    },
+    gridList: {
+      display: 'flex',
+      flexWrap: 'nowrap',
+      overflowX: 'auto',
+    },
+    titleStyle: {
+      color: '#000000',
+    },
+  };
+  
+  
 class CategoryList extends Component {
     render() {
         const {categories} = this.props
         const categoriesWrapper = categories.map((category, i) => (
-            <ListItem
-                primaryText={category.name} />
+            <GridTile
+                title={category.name}
+                titleStyle={styles.titleStyle}
+                // titleBackground="linear-gradient(to top, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+            >
+            {/* <img src={tile.img} /> */}
+          </GridTile>
         ));
         return(
             <div className="categoryList">
-                Category List
-                <List>
+                <GridList style={styles.gridList} cols={2.2}>
                     {categoriesWrapper}
-                </List>
+                </GridList>
             </div>
         );    
     }
