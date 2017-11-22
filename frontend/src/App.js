d8ec089ee24d8dedbd650737d2c796a25795ba67
Sriram Poondi Chinappa <2k6sriram@gmail.com>
2017-11-21 23:40:55 -0700

    Adds PostsList component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,13 @@ import { connect } from 'react-redux';
 import CategoryList from './components/categoryList';
 import { getAllCategories } from './actions/categoryActions'
 
+import PostsList from './components/postsList';
+
 import IconButton from 'material-ui/IconButton';
 import ActionHome from 'material-ui/svg-icons/action/home';
 import AppBar from 'material-ui/AppBar';
 import './App.css';
+
 class App extends Component {
   componentDidMount() {
     this.props.fetchAllCategories();
@@ -23,6 +26,7 @@ class App extends Component {
         <AppBar title="Blogster" iconElementLeft={<IconButton tooltip="Home"><Link className="appLink" to="/"><ActionHome style={{'marginRight': '24'}}/></Link></IconButton>} style={{'textAlign': 'center'}}/>
         <Switch>
           <Route exact path="/" render={() => <CategoryList categories={allCategories}/>}/>
+          <Route exact path="/categories/:categoryid" render={() => <PostsList/>}/>
         </Switch>
       </div>
     );
---
+++ frontend/src/components/postsList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+class PostsLists extends Component {
+    render() {
+        return (
+            <div>Helllo This is a post</div>
+        )
+    }
+}
+
+export default PostsLists;
