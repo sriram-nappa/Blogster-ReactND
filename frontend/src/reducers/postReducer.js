d93a08b7edc15229f1e6a345418f4b4912e2c1d0
Sriram Poondi Chinappa <2k6sriram@gmail.com>
2017-11-26 19:03:19 -0700

    Adds reducer cases for posts

--- frontend/src/reducers/postReducer.js
+++ frontend/src/reducers/postReducer.js
@@ -1,4 +1,11 @@
-import { GET_POSTS } from './../actions/postActions';
+import { 
+    GET_POSTS,
+    ADD_POST,
+    UPDATE_POST,
+    DELETE_POST,
+    UNLIKE_POST,
+    LIKE_POST
+} from './../actions/postActions';
 
 const initialState = {
     posts: [],
@@ -7,6 +14,14 @@ const initialState = {
 export default (state= initialState, action) => {
     switch (action.type) {
         case GET_POSTS:
+        case ADD_POST:
+        case DELETE_POST:
+        case UNLIKE_POST:
+        case LIKE_POST:
+        case UPDATE_POST:
+            return Object.assign({}, state, {
+                posts: action.posts
+            })
         default:
             return state;
     }
