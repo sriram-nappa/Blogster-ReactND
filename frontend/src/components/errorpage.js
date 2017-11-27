9459c28815dc0e83c34b4257f130eb43862c2327
Sriram Poondi Chinappa <2k6sriram@gmail.com>
2017-11-26 19:04:40 -0700

    Adds 404 Page

---
+++ frontend/src/components/errorpage.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Page404 extends Component {
+
+  render() {
+    return (
+      <div className="page-404">
+        <h1>PAGE NOT FOUND</h1>
+      </div>
+    );
+  }
+
+}
+
+export default Page404;
