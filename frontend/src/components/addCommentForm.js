cb0cf7e9879304671a1e311d707bc949ee4ce72d
Sriram Poondi Chinappa <2k6sriram@gmail.com>
2017-12-06 23:20:29 -0700

    Adds Comment Form

---
+++ frontend/src/components/addCommentForm.js
@@ -0,0 +1,111 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import uuid from 'react-native-uuid';
+
+import {Link, withRouter, Switch, Route} from 'react-router-dom';
+
+import {
+    addComment,
+    updateComment
+} from '../actions/commentActions';
+
+class AddPostForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            body: '',
+            author: '',
+            title: ''
+        }
+        this.submitForm = this.submitForm.bind(this);
+    }
+    
+    componentDidMount() {
+        console.log(this.props)
+        const {body, author, title} = this.props.post ? this.props.post : '';
+        this.setState({body, author, title})
+    }
+
+    submitForm = (ev) => {
+        ev.preventDefault()
+        const {id, timestamp, category} = this.props.post
+        const post = Object.assign({}, this.state, {
+            category,
+            id: id || uuid.v4(),
+            timestamp: timestamp || Date.now()
+        });
+        if (!this.props.isEdit) {
+            console.log(post)
+            this.props.addPost(post)
+        } else {
+            this.props.updatePost(post)
+        }
+        this.props.closeModal()
+    }
+
+    handleAuthorUpdate = (ev) => {
+        ev.preventDefault()
+        this.setState({author: ev.target.value})
+    }
+
+    handleTitleUpdate = (ev) => {
+        ev.preventDefault()
+        this.setState({title: ev.target.value})
+    }
+
+    handleDescriptionUpdate = (ev) => {
+        ev.preventDefault()
+        this.setState({body: ev.target.value})
+    }
+
+    render() {
+        const {closeModal} = this.props;        
+        return (
+            <div>
+                <form>
+                    {
+                        this.props.isEdit ? null
+                        : <div>
+                            <label>Author : </label>
+                            <TextField name="author" onChange={this.handleAuthorUpdate} hintText="Name" />
+                        </div>
+                    }
+                    <div>
+                        <label>Title : </label>
+                        <TextField name="title" onChange={this.handleTitleUpdate} hintText="Title" />
+                    </div>
+                    <div>
+                        <label>Description : </label>
+                        <TextField name="body" onChange={this.handleDescriptionUpdate} hintText="Description" />
+                    </div>
+                    <FlatButton
+                        label="Submit"
+                        primary={true}
+                        onClick={this.submitForm}
+                    />
+                    <FlatButton
+                        label="Cancel"
+                        primary={true}
+                        onClick={closeModal}
+                    />
+                </form>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    console.log(state)
+    return {}
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        updatePost : (post) => dispatch(updatePost(post)),
+        addPost : (post) => dispatch(addPost(post)),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddPostForm)
